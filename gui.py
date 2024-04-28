6af6ffe2de72709096a7e7f63955445eb255dfa5
Maple <wang-jiaxun@outlook.com>
2024-04-28 12:18:16 +0800

    remove cef

--- gui.py
+++ gui.py
@@ -1,39 +1,20 @@
-# Hello world example. Doesn't depend on any third party GUI framework.
-# Tested with CEF Python v57.0+.
-#
-# ==== High DPI support on Windows ====
-# To enable DPI awareness on Windows you have to either embed DPI aware manifest
-# in your executable created with pyinstaller or change python.exe properties manually:
-# Compatibility > High DPI scaling override > Application.
-# Setting DPI awareness programmatically via a call to cef.DpiAware.EnableHighDpiSupport
-# is problematic in Python, may not work and can cause display glitches.
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtWebEngineWidgets import *
 
-from cefpython3 import cefpython as cef
-import platform
 import sys
 
 
-def main():
-    check_versions()
-    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
-    cef.DpiAware.EnableHighDpiSupport()
-    cef.Initialize()
-    cef.CreateBrowserSync(url="https://www.google.com/",
-                          window_title="Hello World!")
-    cef.MessageLoop()
-    cef.Shutdown()
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("http://www.google.com"))
+        self.setCentralWidget(self.browser)
+        self.show()
 
 
-def check_versions():
-    ver = cef.GetVersion()
-    print("[hello_world.py] CEF Python {ver}".format(ver=ver["version"]))
-    print("[hello_world.py] Chromium {ver}".format(ver=ver["chrome_version"]))
-    print("[hello_world.py] CEF {ver}".format(ver=ver["cef_version"]))
-    print("[hello_world.py] Python {ver} {arch}".format(
-           ver=platform.python_version(),
-           arch=platform.architecture()[0]))
-    assert cef.__version__ >= "57.0", "CEF Python v57.0+ required to run this"
-
-
-if __name__ == '__main__':
-    main()
+app = QApplication(sys.argv)
+window = MainWindow()
+app.exec_()
