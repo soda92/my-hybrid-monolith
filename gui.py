2cb1d571244ea27829dd2d0e35cfc93a7a88950f
Maple <wang-jiaxun@outlook.com>
2024-04-28 13:32:53 +0800

    break down

--- gui.py
+++ gui.py
@@ -10,11 +10,17 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("http://www.google.com"))
+        self.browser.setBaseSize(800, 600)
+        self.browser.setUrl(QUrl("http://127.0.0.1:8000/"))
         self.setCentralWidget(self.browser)
         self.show()
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-app.exec_()
+def gui_main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    gui_main()
