d36a9469c0895bc6a92fb2b1a4bc5c5f854853ab
Maple <wang-jiaxun@outlook.com>
2024-05-05 18:35:24 +0800

    fix bug

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from gui import gui_main
 import multiprocessing
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     p = multiprocessing.Process(target=backend)
     p.start()
     gui_main()
